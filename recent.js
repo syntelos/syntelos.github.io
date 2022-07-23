41bf588ad50ae538910415d399cba1430dab46a4
John Pritchard <logicalexistentialism@gmail.com>
2022-07-22 21:31:49 -0400

    catalog

--- recent.js
+++ recent.js
@@ -4,127 +4,23 @@
  * CC-BY-NC https://creativecommons.org/licenses/by-nc/4.0/
  */
 
-var recent_schedule = 10000;
-
 /*
- * YYYYMMDD symbolic reference to recent_directory source
- * text within "/recent/YYYMMDD.json".
+ * Paging period in milliseconds.
  */
-var recent_configuration = null;
-
+var recent_schedule = 10000;
 /*
- * Array of objects established by "recent.json" and
- * enhanced with element "div" from document.
+ * YYYY catalog reference in "/recent/YYYY/YYYMMDD.json".
+ */
+var recent_catalog = '2022';
+/*
+ * YYYYMMDD directory reference in
+ * "/recent/YYYY/YYYMMDD.json".
  */
 var recent_directory = null;
-
 /*
  * Timeout ID for UX default auto-paging behavior.
  */
 var recent_paging_id = null;
-
-/*
- * UX display re/initialization
- */
-function recent_initialize(){
-
-    if (null != recent_directory){
-
-        var children = document.body.childNodes;
-        var count = children.length;
-        var index;
-        var child;
-
-        for (index = 0; index < count; index++){
-
-            child = children.item(index);
-
-            if ("page text" == child.className){
-
-                if ("page" == child.id){
-
-                    child.style.visibility = 'visible';
-                }
-                else {
-
-                    child.style.visibility = 'hidden';
-                }
-            }
-        }
-
-        var count = recent_directory.length;
-        var pg;
-        var div;
-
-        for (index = 0; index < count; index++){
-
-            pg = recent_directory[index];
-
-            div = document.getElementById(pg.id);
-
-            if (null == div){
-
-                div = document.createElement("div");
-
-                div.id = pg.id;
-                div.className = 'page text';
-                div.style.visibility = 'hidden';
-
-                dl = document.createElement("dl");
-                dl.className = 'text';
-                div.appendChild(dl);
-
-                dt = document.createElement("dt");
-                dt.className = 'text';
-                dl.appendChild(dt);
-
-                if (pg.link && pg.path){
-
-                    a = document.createElement("a");
-                    a.className = 'text';
-                    a.href = pg.link;
-
-                    if (pg.icon){
-                        img = document.createElement("img");
-                        img.className  = 'text';
-                        img.src = '/images/'+pg.icon+'.svg';
-
-                        a.appendChild(img);
-                    }
-
-                    if (pg.path){
-                        txt = document.createElement("span");
-                        txt.className = 'text';
-                        txt.innerText = pg.path;
-
-                        a.appendChild(txt);
-                    }
-
-                    dt.appendChild(a);
-                }
-
-                dd = document.createElement("dd");
-                dd.className = 'text';
-                dl.appendChild(dd);
-
-                if (pg.embed){
-                    ifr = document.createElement("iframe");
-                    ifr.src = pg.embed;
-                    ifr.className = 'embed';
-
-                    dd.appendChild(ifr);
-                }
-                document.body.appendChild(div);
-            }
-
-        }
-
-        if (null == recent_paging_id){
-
-            recent_paging_id = setInterval(recent_page_next,recent_schedule);
-        }
-    }
-}
 
 /*
  * UX display navigation
@@ -139,6 +35,9 @@ function recent_nav_left (){
     recent_page_prev();
 }
 
+/*
+ * UX display navigation
+ */
 function recent_nav_recycle (){
 
     if (recent_paging_id){
@@ -154,6 +53,9 @@ function recent_nav_recycle (){
     }
 }
 
+/*
+ * UX display navigation
+ */
 function recent_nav_right (){
 
     if (recent_paging_id){
@@ -218,6 +120,9 @@ function recent_page_prev (){
 
 }
 
+/*
+ * UX display paging
+ */
 function recent_page_begin (){
     var children = document.body.childNodes;
     var count = children.length;
@@ -295,12 +200,34 @@ function recent_page_next (){
 }
 
 /*
- * Construct 'recent_directory' from
- * '/recent/YYYYMMDD.json'.
+ * <onchange> from <select#catalog>.
  */
-function recent_build (){
+function recent_select_catalog(){
 
-    var selected = false;
+    var select = document.getElementById("catalog");
+    if (select.hasChildNodes()){
+
+        var children = select.childNodes;
+        var count = children.length;
+        var index;
+        for (index = 0; index < count; index++){
+
+            var option = children.item(index);
+
+            if (option.selected && null != option.value && 4 == option.value.length){
+
+                recent_catalog = option.value;
+            }
+        }
+    }
+
+    recent_configure_directory();
+}
+
+/*
+ * <onchange> from <select#directory>.
+ */
+function recent_select_directory(){
 
     var select = document.getElementById("directory");
     if (select.hasChildNodes()){
@@ -313,15 +240,183 @@ function recent_build (){
             var option = children.item(index);
             if (option.selected){
 
-                recent_configuration = option.value;
+                recent_directory = option.value;
 
                 document.location.hash = option.value;
-
-                selected = true;
             }
         }
     }
 
+    recent_configure_pages();
+}
+
+/*
+ * Document body <onload>.
+ */
+function recent_configure(){
+
+    if (null != document.location.hash && 9 == document.location.hash.length){
+
+        recent_directory = document.location.hash.substring(1,9);
+    }
+
+    var catalog = document.getElementById('catalog');
+    if (null != catalog){
+
+        var children = catalog.childNodes;
+        var count = children.length;
+        var index;
+        var child;
+
+        for (index = (count-1); 0 <= index; index--){
+
+            child = children.item(index);
+
+            catalog.removeChild(child);
+        }
+    }
+
+    var catalog_loader = new XMLHttpRequest();
+
+    catalog_loader.open("GET","/recent/index.txt",true);
+
+    catalog_loader.onload = function (e) {
+
+        if (200 == catalog_loader.status && null != catalog_loader.responseText && 4 < catalog_loader.responseText.length){
+            /*
+             */
+            var index_txt = catalog_loader.responseText;
+            var index_ary = index_txt.split('\n');
+
+            if (null == recent_catalog){
+
+                recent_catalog = index_ary[0];
+            }
+            /*
+             */
+            var catalog = document.getElementById('catalog');
+            if (null != catalog){
+
+                var count = index_ary.length;
+                var index;
+                for (index = 0; index < count; index++){
+
+                    var index_value = index_ary[index];
+                    if (4 == index_value.length){
+
+                        var option = document.createElement("option");
+                        option.className = 'text';
+                        option.value = index_value;
+                        option.innerText = index_value;
+
+                        if (index_value == recent_catalog){
+
+                            option.selected = 'true';
+                        }
+
+                        catalog.appendChild(option);
+                    }
+                }
+
+                catalog.onchange = recent_select_catalog;
+            }
+
+            recent_configure_directory();
+        }
+    };
+    catalog_loader.send(null);
+}
+
+/*
+ * UX constructor called from body <onload> and catalog <select>.
+ */
+function recent_configure_directory(){
+
+    var directory = document.getElementById('directory');
+    if (null != directory){
+
+        var children = directory.childNodes;
+        var count = children.length;
+        var index;
+        var child;
+
+        for (index = (count-1); 0 <= index; index--){
+
+            child = children.item(index);
+
+            directory.removeChild(child);
+        }
+    }
+    var directory_loader = new XMLHttpRequest();
+
+    directory_loader.open("GET","/recent/"+recent_catalog+"/index.txt",true);
+
+    directory_loader.onload = function (e) {
+
+        if (200 == directory_loader.status && null != directory_loader.responseText && 8 < directory_loader.responseText.length){
+
+            var index_txt = directory_loader.responseText;
+            var index_ary = index_txt.split('\n');
+
+            if (null == recent_directory){
+
+                recent_directory = index_ary[0];
+            }
+
+            var selection = false;
+
+            var count = index_ary.length;
+            var index;
+            for (index = 0; index < count; index++){
+
+                var index_value = index_ary[index];
+                if (8 == index_value.length){
+
+                    var option = document.createElement("option");
+                    option.className = 'text';
+                    option.value = index_value;
+                    option.innerText = index_value;
+
+                    if (index_value == recent_directory){
+
+                        option.selected = 'true';
+
+                        selection = true;
+                    }
+
+                    directory.appendChild(option);
+                }
+            }
+
+            if (0 < count && (!selection)){
+
+                var selected = index_ary[0];
+
+                recent_directory = selected;
+
+                document.location.hash = selected;
+
+                directory.childNodes.item(0).selected = true;
+            }
+            else if (document.location.hash){
+
+                document.location.hash = null;
+            }
+
+            directory.onchange = recent_select_directory;
+
+            recent_configure_pages();
+        }
+    };
+    directory_loader.send(null);
+
+}
+
+/*
+ * UX constructor called from directory <onchange> and
+ * construction.
+ */
+function recent_configure_pages(){
     {
         var children = document.body.childNodes;
         var count = children.length;
@@ -330,7 +425,7 @@ function recent_build (){
 
         for (index = (count-1); 0 <= index; index--){
 
-            child = document.body.childNodes.item(index);
+            child = children.item(index);
 
             if (null != child && "page text" == child.className && "page" != child.id){
 
@@ -339,118 +434,113 @@ function recent_build (){
         }
     }
 
-    if (null != recent_configuration){
+    if (null != recent_catalog && null != recent_directory){
 
         var directory_loader = new XMLHttpRequest();
 
-        directory_loader.open("GET","/recent/"+recent_configuration+".json",true);
+        directory_loader.open("GET","/recent/"+recent_catalog+'/'+recent_directory+".json",true);
 
         directory_loader.onload = function (e) {
 
             if (200 == directory_loader.status && null != directory_loader.responseText){
 
-                recent_directory = JSON.parse(directory_loader.responseText);
+                var directory = JSON.parse(directory_loader.responseText);
 
-                recent_initialize();
+                var children = document.body.childNodes;
+                var count = children.length;
+                var index;
+                var child;
+
+                for (index = 0; index < count; index++){
+
+                    child = children.item(index);
+
+                    if ("page text" == child.className){
+
+                        if ("page" == child.id){
+
+                            child.style.visibility = 'visible';
+                        }
+                        else {
+
+                            child.style.visibility = 'hidden';
+                        }
+                    }
+                }
+
+                var count = directory.length;
+                var pg;
+                var div;
+
+                for (index = 0; index < count; index++){
+
+                    pg = directory[index];
+
+                    div = document.getElementById(pg.id);
+
+                    if (null == div){
+
+                        div = document.createElement("div");
+
+                        div.id = pg.id;
+                        div.className = 'page text';
+                        div.style.visibility = 'hidden';
+
+                        dl = document.createElement("dl");
+                        dl.className = 'text';
+                        div.appendChild(dl);
+
+                        dt = document.createElement("dt");
+                        dt.className = 'text';
+                        dl.appendChild(dt);
+
+                        if (pg.link && pg.path){
+
+                            a = document.createElement("a");
+                            a.className = 'text';
+                            a.href = pg.link;
+
+                            if (pg.icon){
+                                img = document.createElement("img");
+                                img.className  = 'text';
+                                img.src = '/images/'+pg.icon+'.svg';
+
+                                a.appendChild(img);
+                            }
+
+                            if (pg.path){
+                                txt = document.createElement("span");
+                                txt.className = 'text';
+                                txt.innerText = pg.path;
+
+                                a.appendChild(txt);
+                            }
+
+                            dt.appendChild(a);
+                        }
+
+                        dd = document.createElement("dd");
+                        dd.className = 'text';
+                        dl.appendChild(dd);
+
+                        if (pg.embed){
+                            ifr = document.createElement("iframe");
+                            ifr.src = pg.embed;
+                            ifr.className = 'embed';
+
+                            dd.appendChild(ifr);
+                        }
+                        document.body.appendChild(div);
+                    }
+
+                }
+
+                if (null == recent_paging_id){
+
+                    recent_paging_id = setInterval(recent_page_next,recent_schedule);
+                }
             }
         };
         directory_loader.send(null);
     }
-}
-
-/*
- * Construct 'recent_configuration' from UX, catalog and
- * directory selectors.
- */
-function recent_configure (){
-
-    if (null != document.location.hash && 9 == document.location.hash.length){
-
-        recent_configuration = document.location.hash.substring(1,9);
-    }
-
-    var configuration_loader = new XMLHttpRequest();
-    /*
-     * [TODO] catalog
-     */
-    configuration_loader.open("GET","/recent/index.txt",true);
-
-    configuration_loader.onload = function (e) {
-
-        if (200 == configuration_loader.status && null != configuration_loader.responseText && 8 < configuration_loader.responseText.length){
-            /*
-             */
-            var index_txt = configuration_loader.responseText;
-            var index_ary = index_txt.split('\n');
-
-            if (null == recent_configuration){
-
-                recent_configuration = index_ary[0];
-            }
-            /*
-             * [TODO] catalog
-             */
-            var catalog = document.getElementById('catalog');
-            if (null != catalog){
-                var option = document.createElement("option");
-                option.className = 'text';
-                option.value = "2022";
-                option.innerText = "2022";
-                option.selected = 'true';
-
-                catalog.appendChild(option);
-            }
-            /*
-             */
-            var directory = document.getElementById('directory');
-            if (null != directory){
-
-                var selection = false;
-
-                var count = index_ary.length;
-                var index;
-                for (index = 0; index < count; index++){
-
-                    var index_value = index_ary[index];
-                    if (8 == index_value.length){
-
-                        var option = document.createElement("option");
-                        option.className = 'text';
-                        option.value = index_value;
-                        option.innerText = index_value;
-
-                        if (index_value == recent_configuration){
-
-                            option.selected = 'true';
-
-                            selection = true;
-                        }
-
-                        directory.appendChild(option);
-                    }
-                }
-
-                if (0 < count && (!selection)){
-
-                    var selected = index_ary[0];
-
-                    recent_configuration = selected;
-
-                    document.location.hash = selected;
-
-                    directory.childNodes.item(0).selected = true;
-                }
-                else if (document.location.hash){
-
-                    document.location.hash = null;
-                }
-
-                directory.onchange = recent_build;
-            }
-
-            recent_build();
-        }
-    };
-    configuration_loader.send(null);
-
 }
