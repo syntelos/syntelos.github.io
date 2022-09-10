9252c1511d020ad7b64d79f8b2c1f6f648546db1
John Pritchard <logicalexistentialism@gmail.com>
2022-09-10 06:03:31 -0400

    collections

--- collections/collections.js
+++ collections/collections.js
@@ -16,54 +16,24 @@ var collection_name = null;
 /*
  */
 function collection_onload(){
+    /*
+     * Initialization
+     */
+    {
+        var path = document.location.pathname.split('/');
 
-    var path = document.location.pathname.split('/');
-
-    collection_yyyy = path[2];
-    collection_mm = path[3];
-    collection_name = path[4].split('.')[0];
-
-    var collection = document.getElementById('collection');
-
-    var loader = new XMLHttpRequest();
-
-    loader.open("GET","/collections/"+collection_name+".txt",false);
-    loader.onload = function (e){
-
-        if (4 == loader.readyState && 200 == loader.status &&
-            null != loader.responseText)
+        collection_yyyy = path[2];
+        collection_mm = path[3];
+        collection_name = path[4].split('.')[0];
+    }
+    /*
+     * Decoration
+     */
+    {
+        var script = document.createElement('script');
         {
-            var index_txt = loader.responseText;
-            var index_ary = index_txt.split('\n');
-            var index_cnt = index_ary.length;
-            var index;
-            for (index = 0; index < index_cnt; index++){
-
-                var index_record = index_ary[index].split('\t');
-
-                    try {
-                        var tweet_id = new URL(index_record[0]);
-                        var tweet_url = new URL(index_record[1]);
-
-                        var tweet_path = tweet_url.pathname.split('/');
-                        var tweet_poster = tweet_path[1];
-                        var tweet_id = tweet_path[3];
-
-                        var twel_li = document.createElement('li');
-                        {
-                            twel_li.id = (tweet_poster+'-'+tweet_id);
-
-                            collection.appendChild(twel_li);
-                        }
-
-
-                    }
-                    catch (x){
-                        console.error(x);
-                    }
-                }
-            }
+            script.src = "https://platform.twitter.com/widgets.js";
         }
-    };
-    loader.send();
+        document.body.appendChild(script);
+    }
 }
