b87bf2174c1e8e58fab8ba7c1bf2960729bb1c8e
John Pritchard <logicalexistentialism@gmail.com>
2023-12-02 12:03:44 -0500

    recent

---
+++ update.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if 2>/dev/null tgtd=recent/$(yyyy)/$(mm) && 2>/dev/null tgtf=${tgtd}/$(yyyymmdd).json
+then
+    if [ ! -d "${tgtd}" ]
+    then
+	mkdir -p "${tgtd}"
+    fi
+
+    if recent gdr jsw "${tgtf}"
+    then
+	git add "${tgtf}"
+	./index.sh
+    else
+	1>&2 echo "$0: error from 'recent gdr jsw ${tgtf}'."
+	exit 1
+    fi
+else
+    1>&2 echo "$0: error from 'tgtf=recent/$(yyyy)/$(mm)/$(yyyymmdd).json'."
+    exit 1
+fi
