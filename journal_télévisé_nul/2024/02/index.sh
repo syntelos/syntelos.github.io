29e0e082c6da94775f956a374d78eb330ff5a591
John Pritchard <logicalexistentialism@gmail.com>
2024-02-29 08:32:21 -0500

    journal télévisé nul

--- journal_télévisé_nul/2024/02/index.sh
+++ journal_télévisé_nul/2024/02/index.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -x
+
 target=$(yyyymmdd).json
 cat<<EOF>${target}
 [
@@ -8,6 +8,7 @@ list=$(ls *.svg *.png)
 tail=$(ls *.svg *.png | tail -n 1 ) 
 for src in ${list}
 do
+    name=$(echo ${src} | sed 's%\.[a-z]*%%')
     tablename=$(echo ${src} | sed 's/-.*//')
     datetime=$(echo ${src} | sed 's%.*-%%; s%\.png%%;')
     yyyy=$(echo ${datetime} | sed 's%_.*%%; s%....$%%;')
@@ -20,7 +21,7 @@ do
         "icon": "syntelos-catalog",
         "path": "${tablename}",
         "link": "https://drive.google.com/drive/folders/1r12NAOW-14pcXRNnlGsvUXGCAx42SyAy?usp=drive_link",
-	"name": "${src}",
+	"name": "${name}",
         "embed": "/${tablename}/${yyyy}/${mm}/${src}"
     }
 EOF
@@ -31,7 +32,7 @@ EOF
         "icon": "syntelos-catalog",
         "path": "${tablename}",
         "link": "https://drive.google.com/drive/folders/1r12NAOW-14pcXRNnlGsvUXGCAx42SyAy?usp=drive_link",
-	"name": "${src}",
+	"name": "${name}",
         "embed": "/${tablename}/${yyyy}/${mm}/${src}"
     },
 EOF
